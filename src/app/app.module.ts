faebe4ca63cd6135da7dd4b0ab483e1231942e7e
vivian <vivy27@gmail.com>
2017-04-14 11:11:08 +0530

    added by vivian

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { SignupComponentComponent } from './signup-component/signup-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponentComponent,
+    SignupComponentComponent
   ],
   imports: [
     BrowserModule,

0ffbcca185e4102d796c0ef0bb63c048d411b8a1
vivian <vivy27@gmail.com>
2017-04-14 22:17:03 +0530

    password validation added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -31,7 +31,7 @@ export class LoginComponent implements OnInit {
 	  let options = new RequestOptions({ headers: headers });
 	
 	  this.http
-		.post('http://46.38.242.27:8082/api/login', body)
+		.post('http://46.38.242.27:8082/api/login', body, options)
 		.map(response => response.json())
 		.subscribe(
 		  response  => console.log(response),

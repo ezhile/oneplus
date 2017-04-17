d3aa5b250107cdd5e35e6912ca2fceb404acd9ed
ezhile <ezhilengels12989@gmail.com>
2017-04-17 23:19:05 +0530

    email subscribe error validation

--- src/app/subscribe/subscribe.component.ts
+++ src/app/subscribe/subscribe.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Http, Headers, RequestOptions  } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { environment } from '../../environments/environment';
 import { emailSubscribe }    from './emailSubscribe';
 
 @Component({
@@ -17,6 +18,8 @@ export class SubscribeComponent implements OnInit {
   model = new emailSubscribe('');  
   submitted = false;
   errorMessage = "";
+  isEmailSubscribed= false;
+  isEmailSubscribedError= false;
 	
   onSubmit() {
     this.submitted = true;
@@ -24,19 +27,20 @@ export class SubscribeComponent implements OnInit {
   }
   
   authenticate(userEmail) {
-	  let body = { 
-		  "email" : userEmail
-		}
+	  let body = { "email" : userEmail };
 	  let headers = new Headers({ 'Content-Type': 'application/json' });
 	  let options = new RequestOptions({ headers: headers });
-	
-	  this.http
-		.post('http://46.38.242.27:8082/api/subscribe', body, options)
+	   this.http[environment.api.subscribe.method]
+		(environment.api.subscribe.url, JSON.stringify(body), options)
 		.map(response => response.json())
 		.subscribe(
-		  response  => {console.log(response);this.submitted = false;},
-          error =>  {this.errorMessage = <any>error;this.submitted = false;}
+		  response  => {console.log(response);this.submitted = false;this.isEmailSubscribed= true;this.isEmailSubscribedError= false;},
+		  error =>  {this.errorMessage = <any>error;this.submitted = false;this.isEmailSubscribed= false;this.isEmailSubscribedError= true;}
 		);
+	}
+	onChangeEmailSubcrbibeInp(){
+		this.isEmailSubscribed= false;
+		this.isEmailSubscribedError= false;
 	}
 
 }

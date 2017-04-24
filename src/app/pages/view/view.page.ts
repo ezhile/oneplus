b58c2050855868cc4ba032a9894693563ef2171b
ezhile <ezhilengels12989@gmail.com>
2017-04-24 18:52:08 +0530

    profile view page data binding from api.

--- src/app/pages/view/view.page.ts
+++ src/app/pages/view/view.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Headers, RequestOptions  } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { environment } from '../../../environments/environment';
+
+
 
 @Component({
   selector: 'view-page',
@@ -7,11 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewPage implements OnInit {
     
-    constructor() {
+    constructor(private http: Http) {
         
     }
     ngOnInit() {
-        
+        this.loadViewPage();
     }	
-	isCustomer=true;
+	userViewObj={};
+	userViewProfile={};
+	userViewLocation={};
+	userViewRate={};
+	isCustomer=false;
+	userServiceMessages={};
+	userServiceMsgText='';
+	loadViewPage(){
+		 this.http[environment.api.profileView.method]
+        (environment.api.profileView.url)
+		.map(response => response.json())
+		.subscribe(
+		  response  => {
+			    
+				this.userViewObj=response;
+				this.userViewProfile=response.profile;	
+				this.userViewLocation=response.profile.location;
+				this.userServiceMessages=response.profile.services;
+				this.userViewRate=response.profile.rate;
+				this.updateUserServiceMessage();
+				
+		  },
+          error =>  {}
+		);
+	}
+	updateUserServiceMessage(){
+		const obj=this.userServiceMessages;
+		this.userServiceMsgText='';
+		//category name
+		for(let i in obj){
+		
+				this.userServiceMsgText=this.userServiceMsgText + obj[i].category+' '+obj[i].name+' ';
+			
+		}
+		console.log(this.userServiceMsgText);
+	}
 }

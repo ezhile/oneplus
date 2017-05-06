1e5f0025d30441d1c4e80b9ea5715759486563fb
vivian <vivy27@gmail.com>
2017-05-06 15:05:08 +0530

    photo upload directive completed

--- src/app/directives/photo-upload.directive.ts
+++ src/app/directives/photo-upload.directive.ts
@@ -1,13 +1,45 @@
-import { Directive, ElementRef, Input, Renderer, OnChanges} from '@angular/core';
+import { Directive, ElementRef, Renderer, Output, EventEmitter, HostListener, OnInit} from '@angular/core';
+declare  var $:any;
 
 @Directive({ selector: '[photo-upload]' })
-export class PhotoUploadDirective implements OnChanges {
+export class PhotoUploadDirective implements OnInit {
 
-    constructor(public el: ElementRef, public renderer: Renderer) {}
+    private nativeElement : Node;
+    public inputElement : Element;
 
-    @Input() starRating: number;
+    @Output() onComplete: EventEmitter<string> = new EventEmitter<string>();
 
-    ngOnChanges(){
-        
+    
+    constructor(public el: ElementRef, public renderer: Renderer) {
+        this.nativeElement =  el.nativeElement;
+    }
+
+    @HostListener('click') onClick() {
+        $(this.inputElement).trigger('click');
+    }
+
+    ngOnInit(){
+        this.inputElement = this.renderer.createElement(this.nativeElement, "input");
+        this.renderer.setElementAttribute(this.inputElement, "style", "display:none;");
+        this.renderer.setElementAttribute(this.inputElement, "type", "file");
+        this.renderer.setElementAttribute(this.inputElement, "accept", "image/x-png,image/gif,image/jpeg");
+        this.renderer.listen(this.inputElement, "change", ( event ) => this.onImageUpload(event));
+    }
+    onImageUpload(e){
+        let files = e.target.files;
+        let file = files[0];
+    
+        if (files && file) {
+            let reader = new FileReader();
+
+            reader.onload =this.handleReaderLoaded.bind(this);
+
+            reader.readAsBinaryString(file);
+        }
+    }
+    handleReaderLoaded(readerEvt) {
+        let binaryString = readerEvt.target.result;
+        let base64textString= btoa(binaryString);
+        this.onComplete.emit(base64textString);
     }
 }

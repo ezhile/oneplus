9f7625c3b10596b2d55c5b670e8425e51308b4ad
vivian <vivy27@gmail.com>
2017-06-11 16:57:25 +0530

    clock picker corrected

--- src/app/directives/clock-picker.directive.ts
+++ src/app/directives/clock-picker.directive.ts
@@ -1,12 +1,39 @@
-import { Directive, ElementRef, Input, Renderer } from '@angular/core';
+import { Directive, ElementRef, forwardRef, Renderer } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 declare  var $:any;
 
-@Directive({ selector: '[clock-picker]' })
-export class ClockPickerDirective {
+@Directive({
+  selector: '[clock-picker]',
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,useExisting: 
+    forwardRef(() => ClockPickerDirective),
+    multi: true
+  }]
+})
+export class ClockPickerDirective implements ControlValueAccessor {
+    value: string;
 
-    constructor(public el: ElementRef, public renderer: Renderer) {}
+    constructor(protected el: ElementRef) {}
 
-    ngOnInit(){
-        $(this.el.nativeElement).clockpicker();
+    ngAfterViewInit(){
+        $(this.el.nativeElement).clockpicker().on('change', e => {
+            this.onModelChange(e.target.value);
+        });
+    }
+    
+    onModelChange: Function = () => {};
+    
+    onModelTouched: Function = () => {};
+
+    writeValue(val: string) : void {
+        this.value = val;
+    }
+    
+    registerOnChange(fn: Function): void {
+        this.onModelChange = fn;
+    }
+
+    registerOnTouched(fn: Function): void {
+        this.onModelTouched = fn;
     }
 }
